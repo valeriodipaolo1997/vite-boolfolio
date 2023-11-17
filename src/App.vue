245bb376ec7e660e20ca2f14ccf19825e94a74d7
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-11-17 17:20:50 +0100

    i axios and recover projects from mine back-end

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+export default {
+  name: 'App',
+  data() {
+    return {
+      base_url: 'http://127.0.0.1:8000',
+      projects_api: '/api/projects',
+      projects: [],
+    }
+  },
+  methods: {
+    getProjects() {
+      axios
+        .get(this.base_url + this.projects_api)
+        .then(response => {
+          console.log(response);
+          this.projects = response.data.result
+        })
+        .catch(error => {
+          console.error(error);
+        })
+    }
+  },
+  mounted() {
+    this.getProjects()
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+   <div class="container">
+    <div class="row ">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5">
+        <div class="card">
+
+          <img :src="project.thumb" alt="">
+
+          <h3>{{ project.title }}</h3>
+
+          <div>{{ project.description }}</div>
+        </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
