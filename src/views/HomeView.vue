34d0f547cd2ddf2a39e05f48733cfc3aa9b21201
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-11-24 15:09:50 +0100

    add latest project in home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,38 @@
 <script>
+
+import ProjectCard from '../components/ProjectCard.vue';
+import { state } from '../state.js';
 export default {
-    name: 'HomeView'
+    name: 'HomeView',
+    data() {
+        return {
+            state
+        }
+    },
+    components: {
+        ProjectCard
+    },
+    mounted() {
+        this.state.getLatestProjects()
+    }
 }
 </script>
 
-<template>
-    <div>
-        <h1> Home</h1>
 
-    </div>
+    
+
+
+
+<template>
+    <section class="latest py-5 bg-secondary">
+        <div class="container">
+            <h1 class="text-uppercase text-center py-4 text-primary">My latest projects</h1>
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
+                <ProjectCard :project="project" v-for="project in this.state.latestProjects" />
+            </div>
+        </div>
+
+    </section>
 </template>
 
 
